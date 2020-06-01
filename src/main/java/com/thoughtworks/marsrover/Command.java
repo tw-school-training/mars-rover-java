d9dff75c87eb6379f61dd4c83b7db7c6686b3ac8
vickerx <vicker23@163.com>
2020-06-01 15:00:05 +0800

    Write code for command M & facing E

--- src/main/java/com/thoughtworks/marsrover/Command.java
+++ src/main/java/com/thoughtworks/marsrover/Command.java
@@ -2,15 +2,26 @@ package com.thoughtworks.marsrover;
 
 public class Command {
 
+    private String command;
+
     public Command() {
 
     }
 
     public Command(String command) {
-
+        this.command = command;
     }
 
     public Position execute(Position current) {
-        return current;
+        if (command == null) {
+            return current;
+        }
+
+        Position result = current;
+
+        if ("M".equals(command) && current.getDirection() == Direction.E) {
+            result = Position.builder().x(current.getX() + 1).y(current.getY()).direction(current.getDirection()).build();
+        }
+        return result;
     }
 }
