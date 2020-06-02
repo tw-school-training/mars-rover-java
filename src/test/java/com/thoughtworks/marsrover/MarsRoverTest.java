681453b0df8ae967790c4467ff7c03091fd1b3bd
vickerx <vicker23@163.com>
2020-06-02 10:20:18 +0800

    return facing W given command R and facing S

--- src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
+++ src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
@@ -124,4 +124,16 @@ public class MarsRoverTest {
         Assert.assertEquals(0, position.getY());
         Assert.assertEquals(Direction.S, position.getDirection());
     }
+
+    @Test
+    public void should_return_facing_W_given_command_is_R_and_facing_is_S() {
+        MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.S).build());
+
+        Command command = new Command("R");
+        Position position = marsRover.execute(command);
+
+        Assert.assertEquals(0, position.getX());
+        Assert.assertEquals(0, position.getY());
+        Assert.assertEquals(Direction.W, position.getDirection());
+    }
 }
