3f59727a5065c72e6a61e6fbd804c1e9e5356bd3
vickerx <vicker23@163.com>
2020-06-05 15:56:06 +0800

    Remove Command

--- src/main/java/com/thoughtworks/marsrover/MarsRover.java
+++ src/main/java/com/thoughtworks/marsrover/MarsRover.java
@@ -12,8 +12,93 @@ public class MarsRover {
         current = position;
     }
 
-    public Position execute(Command command) {
-        current = command.execute(current);
-        return current;
+    public Position execute(String command) {
+        int index = command.indexOf("\n");
+        String behaviorCommand = command;
+        if (index > -1) {
+            String initializedCommand = command.substring(0, index);
+            current = init(initializedCommand);
+            behaviorCommand = command.substring(index + 1);
+        }
+
+        Position result = current;
+        for (int commandIndex = 0; commandIndex < behaviorCommand.length(); commandIndex++) {
+            switch (behaviorCommand.charAt(commandIndex)) {
+                case 'M':
+                    result = move(result);
+                    break;
+                case 'L':
+                    result = left(result);
+                    break;
+                case 'R':
+                    result = right(result);
+                    break;
+            }
+        }
+
+        return result;
+    }
+
+    private Position init(String initializedCommand) {
+        String[] position = initializedCommand.split(" ");
+        return Position.builder()
+                .x(Integer.parseInt(position[0]))
+                .y(Integer.parseInt(position[1]))
+                .direction(Direction.valueOf(position[2])).build();
+    }
+
+    private Position right(Position current) {
+        Direction newDirection = current.getDirection();
+        switch (current.getDirection()) {
+            case E:
+                newDirection = Direction.S;
+                break;
+            case S:
+                newDirection = Direction.W;
+                break;
+            case W:
+                newDirection = Direction.N;
+                break;
+            case N:
+                newDirection = Direction.E;
+        }
+        return Position.builder().x(current.getX()).y(current.getY()).direction(newDirection).build();
+    }
+
+    private Position left(Position current) {
+        Direction newDirection = current.getDirection();
+        switch (current.getDirection()) {
+            case E:
+                newDirection = Direction.N;
+                break;
+            case N:
+                newDirection = Direction.W;
+                break;
+            case W:
+                newDirection = Direction.S;
+                break;
+            case S:
+                newDirection = Direction.E;
+        }
+        return Position.builder().x(current.getX()).y(current.getY()).direction(newDirection).build();
+    }
+
+    private Position move(Position current) {
+        int x = current.getX();
+        int y = current.getY();
+        switch (current.getDirection()) {
+            case E:
+                x++;
+                break;
+            case W:
+                x--;
+                break;
+            case N:
+                y++;
+                break;
+            case S:
+                y--;
+        }
+        return Position.builder().x(x).y(y).direction(current.getDirection()).build();
     }
 }
--- src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
+++ src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
@@ -9,8 +9,7 @@ public class MarsRoverTest {
     public void should_return_init_place_given_initialize_command() {
         MarsRover marsRover = new MarsRover();
 
-        Command command = new Command("1 2 N\n");
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("1 2 N\n");
 
         Assert.assertEquals(1, position.getX());
         Assert.assertEquals(2, position.getY());
@@ -21,8 +20,7 @@ public class MarsRoverTest {
     public void should_return_x_plus_1_given_command_is_M_and_facing_is_E() {
         MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.E).build());
 
-        Command command = new Command("M");
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("M");
 
         Assert.assertEquals(1, position.getX());
         Assert.assertEquals(0, position.getY());
@@ -33,8 +31,7 @@ public class MarsRoverTest {
     public void should_return_x_minus_1_given_command_is_M_and_facing_is_W() {
         MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.W).build());
 
-        Command command = new Command("M");
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("M");
 
         Assert.assertEquals(-1, position.getX());
         Assert.assertEquals(0, position.getY());
@@ -45,8 +42,7 @@ public class MarsRoverTest {
     public void should_return_y_plus_1_given_command_is_M_and_facing_is_N() {
         MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.N).build());
 
-        Command command = new Command("M");
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("M");
 
         Assert.assertEquals(0, position.getX());
         Assert.assertEquals(1, position.getY());
@@ -57,8 +53,7 @@ public class MarsRoverTest {
     public void should_return_y_minus_1_given_command_is_M_and_facing_is_S() {
         MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.S).build());
 
-        Command command = new Command("M");
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("M");
 
         Assert.assertEquals(0, position.getX());
         Assert.assertEquals(-1, position.getY());
@@ -69,8 +64,7 @@ public class MarsRoverTest {
     public void should_return_facing_N_given_command_is_L_and_facing_is_E() {
         MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.E).build());
 
-        Command command = new Command("L");
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("L");
 
         Assert.assertEquals(0, position.getX());
         Assert.assertEquals(0, position.getY());
@@ -81,8 +75,7 @@ public class MarsRoverTest {
     public void should_return_facing_W_given_command_is_L_and_facing_is_N() {
         MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.N).build());
 
-        Command command = new Command("L");
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("L");
 
         Assert.assertEquals(0, position.getX());
         Assert.assertEquals(0, position.getY());
@@ -93,8 +86,7 @@ public class MarsRoverTest {
     public void should_return_facing_S_given_command_is_L_and_facing_is_W() {
         MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.W).build());
 
-        Command command = new Command("L");
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("L");
 
         Assert.assertEquals(0, position.getX());
         Assert.assertEquals(0, position.getY());
@@ -105,8 +97,7 @@ public class MarsRoverTest {
     public void should_return_facing_E_given_command_is_L_and_facing_is_S() {
         MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.S).build());
 
-        Command command = new Command("L");
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("L");
 
         Assert.assertEquals(0, position.getX());
         Assert.assertEquals(0, position.getY());
@@ -117,8 +108,7 @@ public class MarsRoverTest {
     public void should_return_facing_S_given_command_is_R_and_facing_is_E() {
         MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.E).build());
 
-        Command command = new Command("R");
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("R");
 
         Assert.assertEquals(0, position.getX());
         Assert.assertEquals(0, position.getY());
@@ -129,8 +119,7 @@ public class MarsRoverTest {
     public void should_return_facing_W_given_command_is_R_and_facing_is_S() {
         MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.S).build());
 
-        Command command = new Command("R");
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("R");
 
         Assert.assertEquals(0, position.getX());
         Assert.assertEquals(0, position.getY());
@@ -141,8 +130,7 @@ public class MarsRoverTest {
     public void should_return_facing_N_given_command_is_R_and_facing_is_W() {
         MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.W).build());
 
-        Command command = new Command("R");
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("R");
 
         Assert.assertEquals(0, position.getX());
         Assert.assertEquals(0, position.getY());
@@ -153,8 +141,7 @@ public class MarsRoverTest {
     public void should_return_facing_E_given_command_is_R_and_facing_is_N() {
         MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.N).build());
 
-        Command command = new Command("R");
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("R");
 
         Assert.assertEquals(0, position.getX());
         Assert.assertEquals(0, position.getY());
@@ -165,8 +152,7 @@ public class MarsRoverTest {
     public void should_return_correct_location_and_direction_given_command_is_MLMMRMMML_and_facing_is_N() {
         MarsRover marsRover = new MarsRover(Position.builder().x(0).y(0).direction(Direction.N).build());
 
-        Command command = new Command("MLMMRMMML");//y+1,W,x-1,x-1,N,y+1,y+1,y+1,W
-        Position position = marsRover.execute(command);
+        Position position = marsRover.execute("MLMMRMMML");//y+1,W,x-1,x-1,N,y+1,y+1,y+1,W
 
         Assert.assertEquals(-2, position.getX());
         Assert.assertEquals(4, position.getY());
